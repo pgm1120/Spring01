0de1c02c9e24a6433e1e764ae5d4bea01afa7481
pgm1120 <cpzhrkwk755@gmail.com>
2022-06-23 23:29:46 +0900

    test

--- Spring01/src/main/java/com/example/demo/Spring01Application.java
+++ Spring01/src/main/java/com/example/demo/Spring01Application.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Spring01Application {
@@ -9,5 +11,7 @@ public class Spring01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Spring01Application.class, args);
 	}
+	
+	
 
 }
